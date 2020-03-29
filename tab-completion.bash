75fc1dc0fb10e456006b75e21b59350faaf6dcd9
Nikandros <m.nik1997@hotmail.com>
2020-03-29 19:41:12 +0100

    main.py file path converted to a dynamic

--- tab-completion.bash
+++ tab-completion.bash
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 _session_completions()
 {
-  # cd '/Users/nikandrosmavroudakis/Documents/Projects/Session'
+  local main_path="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
   local prev="${COMP_WORDS[COMP_CWORD-1]}"
   COMPREPLY=()
   if [ ${prev} == "-i" ]; then
     local IFS=$','
-    COMPREPLY=($(compgen -W "$(python3 ~/Documents/Projects/Session/main.py -k)"))
+    COMPREPLY=($(compgen -W "$(python3 ${main_path}/main.py -k)"))
     return 0
   elif [[ ${prev} == "-s" || ${prev} == "-r" || ${prev} == "-n" ]]; then
     local IFS=$','
